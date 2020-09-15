0be0a92eeb8f6187c74a66ec5ef967139d1ff584
Aman Bhimani <amanb014@gmail.com>
2020-09-15 14:38:52 -0400

    Added searchComponent template

---
+++ src/components/search/search.component.tsx
@@ -0,0 +1,18 @@
+		import React from 'react';
+
+export type SearchComponentProps = {};
+
+export function SearchComponent(props: SearchComponentProps) {
+
+
+	return (
+		<div className="search-component">
+			<div className="search-bar">
+
+			</div>
+			<div className="search-results">
+
+			</div>
+		</div>
+	);
+}
