ce8439d76c9c3e32c044ce1fdf88e1e6b4d13dac
Aman Bhimani <amanb014@gmail.com>
2020-09-15 14:32:18 -0400

    Added  blue background for home

--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
+import './home.component.scss';
 
 export type HomeComponentProps = {};
 
 export function HomeComponent(props: HomeComponentProps) {
 
 	return (
-		<div>
-			home works!
+		<div className="home-component">
+
 		</div>
 	);
 }
