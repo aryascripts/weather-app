5d7350a085ec3cfb36ca03358ebe4432b3d24777
Aman Bhimani <amanb014@gmail.com>
2020-09-16 16:48:53 -0400

    LocalStorage in home component

---
+++ src/model/LocalStorageItem.enum.ts
@@ -0,0 +1,3 @@
+export enum LocalStorageItem {
+	USER_NAME = 'user'
+};
--- src/pages/home/home.component.tsx
+++ src/pages/home/home.component.tsx
@@ -1,15 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './home.component.scss';
 import { SearchComponent } from '../../components/search/search.component';
+import { LocalStorageItem } from '../../model/LocalStorageItem.enum';
 
 export type HomeComponentProps = {};
 
 export function HomeComponent(props: HomeComponentProps) {
 
+	const [name, setName] = useState('');
+
+	useEffect(() => {
+		const current = localStorage.getItem(LocalStorageItem.USER_NAME) || '';
+		setName(current);
+	});
+
+	const updateName = (event: any) => {
+		const val = event.target.value;
+		setName(val);
+		localStorage.setItem(LocalStorageItem.USER_NAME, val);
+	};
+
 	return (
 		<div className="home-component">
 			<div className="search-wrapper">
-				<h1 className="text-center">hello <input type="text" placeholder="your beautiful name" />.</h1>
+				<h1 className="text-center">hello <input value={name} onChange={updateName} type="text" placeholder="your beautiful name" />.</h1>
 				<SearchComponent />
 			</div>
 		</div>
