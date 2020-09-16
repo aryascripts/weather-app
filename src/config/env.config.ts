5c59adbcff52cfc8bcd7ffd9d4ccba6abef70ada
Aman Bhimani <amanb014@gmail.com>
2020-09-16 17:03:26 -0400

    Axios. configuration, WEatherAPI singleton

---
+++ src/config/env.config.ts
@@ -0,0 +1,9 @@
+export class Environment {
+
+	// NOTE - this is only created for educational purposes
+	// PLEASE MANAGE YOUR KEYS RESPONSIBLY
+	// NEVER COMMIT TO GIT / VCS
+	// PUT KEYS IN BACKEND ENVIRONMENT
+	public static readonly WEATHER_API_KEY = 'setInDeployment';
+
+}
---
+++ src/services/weather.api.ts
@@ -0,0 +1,29 @@
+import axios, { AxiosResponse } from 'axios';
+import { Environment } from '../config/env.config';
+
+class WeatherApiSingleton {
+
+	private readonly baseURI = 'https://api.openweathermap.org';
+	private readonly Weather25 = `${this.baseURI}/data/2.5/weather`;
+
+	private getWeatherUrl(zip: string): string {
+		return `${this.Weather25}/?zip=${zip}&appid=${Environment.WEATHER_API_KEY}`
+	}
+
+
+	getCurrentWeather(zip: string): Promise<any> {
+
+		const url = this.getWeatherUrl(zip);
+		return axios.get(url)
+			.then((response: AxiosResponse<any>) => {
+				// massage the response a bit
+				// convert to your objects or 
+				return response.data;
+			});
+	}
+
+
+}
+
+const WeatherApi = new WeatherApiSingleton();
+export {WeatherApi};
