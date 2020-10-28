b2369d067297d1a996cbdf5f611a18f4a541eba8
Aman Bhimani <amanbhimani@Amans-MacBook-Pro.local>
2020-10-28 14:35:51 -0400

    Updated video 3

--- src/components/Title/Title.tsx
+++ src/components/Title/Title.tsx
@@ -3,13 +3,22 @@ import './Title.scss';
 
 export interface TitleProps {
     userName: string;
+    setUsername: (val: string) => void;
 }
 
 export default function(props: TitleProps) {
 
+    const handleOnUsernameChange = (event: any) => {
+        props.setUsername(event.target.value);
+    }
+
     return (
         <div className="title">
-            <h1>hello _________</h1>
+            <h1>hello</h1>
+            <input className="input-username"
+                onChange={handleOnUsernameChange}
+                value={props.userName}
+                type="text" />
         </div>
     )
 }
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Title from '../../components/Title/Title';
+import { USERNAME_KEY } from '../../config/Variables';
 import './Home.scss';
 
 export interface HomeProps {};
 
 export default function (props: HomeProps) {
+
+    const fromLocalStorage: string = localStorage.getItem(USERNAME_KEY) || '';
+    const [userName, setUsername] = useState<string>(fromLocalStorage);
+
+    useEffect(() => {
+        localStorage.setItem(USERNAME_KEY, userName);
+    }, [userName])
+
     return (
         <div className="home-page">
-            <Title userName={'AmanScripts'}/>
+            <Title 
+                setUsername={setUsername} 
+                userName={userName}/>
         </div>
     )
 };
